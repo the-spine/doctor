583a34c555755f256006478be0cdc7fb7ca59f64
GT020 <gauravtanwar0020@gmail.com>
2023-10-14 22:37:44 +0530

    add basic config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,53 @@
 package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Postgres PostgresConfig `mapstructure:"postgres"`
+	Redis    RedisConfig    `mapstructure:"redis"`
+	Api      ApiConfig      `mapstructure:"api"`
+}
+
+type PostgresConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	DBName   string `mapstructure:"dbname"`
+}
+
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Password string `mapstructure:"password"`
+	DB       int    `mapstructure:"db"`
+}
+
+type ApiConfig struct {
+	Host string `mapstructure:"host"`
+	Port int    `mapstructure:"port"`
+}
+
+func LoadConfig(configPath string) (*Config, error) {
+	// Set up Viper
+	viper.AddConfigPath(configPath)
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	// Read config file
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal config into struct
+	var config Config
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
