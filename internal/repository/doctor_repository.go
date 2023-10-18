4d84670d2d0d2345a4e6450ee10d7528b6fed764
GT020 <gauravtanwar0020@gmail.com>
2023-10-18 21:49:27 +0530

    add basic repo functions

--- internal/repository/doctor_repository.go
+++ internal/repository/doctor_repository.go
@@ -1,1 +1,32 @@
 package repository
+
+import (
+	"doctor/internal/db"
+	"doctor/internal/models"
+)
+
+func GetDoctorByID(id string) (models.Doctor, error) {
+	var doctor models.Doctor
+	result := db.DB.Where("id = ?", id).First(&doctor)
+	return doctor, result.Error
+}
+
+func GetDoctors(doctors *[]models.Doctor) error {
+	result := db.DB.Find(doctors)
+	return result.Error
+}
+
+func CreateDoctor(doctor *models.Doctor) error {
+	result := db.DB.Create(doctor)
+	return result.Error
+}
+
+func UpdateDoctor(doctor *models.Doctor) error {
+	result := db.DB.Save(doctor)
+	return result.Error
+}
+
+func DeleteDoctor(id string) error {
+	result := db.DB.Delete(&models.Doctor{}, id)
+	return result.Error
+}
