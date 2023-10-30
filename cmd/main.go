cf4ceaa85a89d1d85cf9a91029959ee38cd5fa8a
GT020 <gauravtanwar0020@gmail.com>
2023-10-30 20:27:54 +0530

    start the server

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"doctor/internal/config"
 	"doctor/internal/db"
+	"doctor/internal/service"
+	"log"
 )
 
 func init() {
@@ -25,4 +27,14 @@ func init() {
 	}
 }
 
-func main() {}
+func main() {
+	log.Println("Starting Grpc Server")
+
+	_, err := service.StartGrpcService(&config.Konfig)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Grpc Server Running")
+}
